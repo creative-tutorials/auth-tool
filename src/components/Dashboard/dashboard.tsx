cd0494def3669ce653e25fcb23b8cc12ef68fc62
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-09-08 21:11:47 +0100

    ✨ 🐛 Fixed Bugs & Introduced new features

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,44 +1,70 @@
+import { AddSession } from "../AddSession/addSession";
 import "../../styles/dash.css";
-import { useEffect, useRef } from "react";
+import "../../styles/sessionDiv.css";
+import email from "/email.svg";
+import facebook from "/facebook-logo-2019.svg";
+import google from "/google.svg";
+import twitter from "/twitter.svg";
+import { useState, useEffect, useRef } from "react";
 
 const dashboardComponent = () => {
+  let isLoaded = false;
+  const ComponentDiv = useRef();
   /* A reference to the div with the class name `_c9`. */
   const div = useRef();
 
   useEffect(() => {
     return () => {
-      /* Fetching data from the API and setting the state of the component. */
       LoadContent();
+      /* Fetching data from the API and setting the state of the component. */
     };
   }, []);
 
   const LoadContent = async () => {
+    isLoaded = true;
     /* Setting the headers of the request. */
     let headersList = {
       "Content-Type": "application/json",
       method: "GET",
     };
+    if (isLoaded) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          successFunction,
+          errorFunction
+        );
+      } else {
+        alert(
+          "It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it."
+        );
+      }
+      async function successFunction(position: any) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        /* Fetching data from the API. */
+        await fetch(
+          `http://localhost:5000/api/sessions/${lat}/${long}`,
+          headersList
+        )
+          .then((response) => response.json())
+          .then((result) => {
+            const sessionObj = result.sessionObj;
+            const location = result.data;
+            /**
+             * The function takes an array of objects and returns an array of objects with the same keys
+             * but with the values of the keys converted to strings.
+             * </code>
+             * @param {any} arr - any -&gt; this is the array that you want to map
+             */
+            const newArr = sessionObj.map(myFunction);
 
-    /* Fetching data from the API. */
-    await fetch("http://localhost:5000/api/sessions", headersList)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        /**
-         * The function takes an array of objects and returns an array of objects with the same keys
-         * but with the values of the keys converted to strings.
-         * </code>
-         * @param {any} arr - any -&gt; this is the array that you want to map
-         */
-        const newArr = result.map(myFunction);
-
-        function myFunction(arr: any) {
-          /* Getting the current value of the ref. */
-          const isCurrent = div.current;
-          /* Creating a new div element. */
-          const createDiv = document.createElement("div");
-          createDiv.className = `${"content"}`
-          createDiv.innerHTML = `<div class="_session_service">
+            function myFunction(arr: any) {
+              const createDiv = document.createElement("div");
+              /* Getting the current value of the ref. */
+              const isCurrent = div.current;
+              /* Creating a new div element. */
+              createDiv.className = `${"content"}`;
+              createDiv.innerHTML = `<div class="_session_service">
           <i class="fa-solid fa-shield-keyhole"></i>
           ${arr.service}
         </div>
@@ -46,17 +72,34 @@ const dashboardComponent = () => {
           <i class="fa-regular fa-lock-keyhole"></i> ${arr.status}
         </div>
         <div class="_session_location">
-          <i class="fa-solid fa-location-dot"></i> ${arr.location}
+          <i class="fa-solid fa-location-dot"></i> ${location}
         </div>`;
-        /* Appending the `createDiv` element to the `isCurrent` element. */
-        isCurrent.appendChild(createDiv)
-          /* Returning the array of objects. */
-          return console.log(arr);
-        }
-      })
-      /* Catching any errors that may occur during the fetching of data from the API. */
-      .catch((error) => console.log("error"));
+              /* Appending the `createDiv` element to the `isCurrent` element. */
+              isCurrent.appendChild(createDiv);
+              /* Returning the array of objects. */
+              return console.log(arr);
+            }
+          })
+          /* Catching any errors that may occur during the fetching of data from the API. */
+          .catch((error) => {
+            console.log(error);
+            isLoaded = false;
+          });
+      }
+      async function errorFunction() {
+        return console.error("You denied access to this application");
+      }
+    }
+    if (!isLoaded) {
+      return console.error("Error loading content");
+    }
+    console.log("loadState", isLoaded);
   };
+  const toggleAddButton = () => {
+    const newComponent = ComponentDiv.current;
+    newComponent.classList.add("active");
+  };
+
   return (
     <div className="_dashboardComponent">
       <div className="_dash_content">
@@ -119,22 +162,26 @@ const dashboardComponent = () => {
                   <div id="name">Location</div>
                 </div>
                 <div className="_c9" ref={div}>
-                  {/* <div className="_session_service">
-                    <i className="fa-solid fa-shield-keyhole"></i>
-                    {serviceData}
-                  </div>
-                  <div className="_session_status">
-                    <i className="fa-regular fa-lock-keyhole"></i> {status}
-                  </div>
-                  <div className="_session_location">
-                    <i className="fa-solid fa-location-dot"></i> {location}
-                  </div> */}
+                  {/*  */}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="addButton">
+        <button id="add_session" onClick={toggleAddButton}>
+          Add New Session
+        </button>
+      </div>
+      <AddSession
+        ComponentDiv={ComponentDiv}
+        email={email}
+        facebook={facebook}
+        google={google}
+        twitter={twitter}
+        text={"Add new Session"}
+      />
     </div>
   );
 };
