36cf1fdb3220d1953aa44a4b5582995db607e207
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-09-09 16:31:13 +0100

    Added new component

--- src/components/Dashboard/dashboard.tsx
+++ src/components/Dashboard/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from "../views/sidebar";
 import { AddSession } from "../AddSession/addSession";
 import "../../styles/dash.css";
 import "../../styles/sessionDiv.css";
@@ -106,47 +107,7 @@ const dashboardComponent = () => {
     <div className="_dashboardComponent">
       <div className="_dash_content">
         <div className="cols">
-          <div className="col-left">
-            <div className="_app_name">App</div>
-            <div className="list_components">
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-grid-2"></i>
-                </div>
-                <div className="_list_text">Dashboard</div>
-              </div>
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-folder-gear"></i>
-                </div>
-                <div className="_list_text">Admin</div>
-              </div>
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-key"></i>
-                </div>
-                <div className="_list_text">Key Generate</div>
-              </div>
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-mask"></i>
-                </div>
-                <div className="_list_text">Sessions</div>
-              </div>
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-gear"></i>
-                </div>
-                <div className="_list_text">Settings</div>
-              </div>
-              <div className="_list">
-                <div className="_list_icon">
-                  <i className="fa-solid fa-wallet"></i>
-                </div>
-                <div className="_list_text">Go Pro</div>
-              </div>
-            </div>
-          </div>
+          <Sidebar />
           <div className="col-right_s1">
             <div className="_s1_tp">
               <div className="_tp_greet">
